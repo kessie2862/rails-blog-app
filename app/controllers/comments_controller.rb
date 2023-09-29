f11ec173cf10785b4078a748a1cdceb20eee9906
kessie <kayp3477@gmail.com>
2023-09-29 15:02:15 +0000

    Implement create action to handle comment creation and association with posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(current_user, @post), notice: 'Comment added successfully.'
+    else
+      redirect_to user_post_path(current_user, @post), alert: 'Comment not saved.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
