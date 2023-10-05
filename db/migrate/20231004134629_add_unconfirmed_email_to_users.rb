bb3d34fcd3e4b9a9f8f33ce1c8cbd78779d2dc4e
kessie <kayp3477@gmail.com>
2023-10-05 21:32:10 +0000

    Deleted unwanted migration files

--- db/migrate/20231004134629_add_unconfirmed_email_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddUnconfirmedEmailToUsers < ActiveRecord::Migration[7.0]
-  def up
-    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
-  end
-
-  def down
-    remove_columns :users, :unconfirmed_email # Only if using reconfirmable
-  end
-end
